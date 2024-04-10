224482c5cac9ea0c8fab73791259d3c84273b5fa
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2024-04-10 12:04:15 +0200

    update

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,25 @@ import { components } from "../slices";
 
 export default function Home({page, settings}) {
 
+  function toggleColor(){
+    document.getElementById('toggle').classList.toggle('active');
+    document.getElementById('content').classList.toggle('color');
+  }
+
   return (
     <>
       <Head>
         <title>{page.data.title}</title>
       </Head>
       <main className={` `}>   
-        <div className="content">
-          <h1>{page.data.title}</h1>
+        <div className="content color" id="content">
+          <div className='header'>
+            <h1>{page.data.title}</h1>
+            <div className='right'>
+              <div className='button' id="toggle" onClick={toggleColor}></div>
+              <h2>Maakt een wereld van verschil</h2>
+            </div>
+          </div>
           <SliceZone slices={page.data.slices} components={components} />
         </div>
       </main>
